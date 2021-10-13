f66ebb8499f0fc862f646cef0e26991b50b75e8f
bdpedigo <benjamindpedigo@gmail.com>
2021-10-13 13:48:22 -0400

    changes to script, fisher's exact

--- pkg/pkg/stats/er_test.py
+++ pkg/pkg/stats/er_test.py
@@ -1,13 +1,32 @@
-from scipy.stats import ttest_ind
+from scipy.stats import ttest_ind, fisher_exact, chi2_contingency
+import numpy as np
 
 
-def erdos_reyni_test(A1, A2, method="t"):
-    # TODO adjust to properly handle the loops
+def erdos_reyni_test(A1, A2, method="fisher"):
+    triu_indices = np.triu_indices_from(A1, k=1)
+    tril_indices = np.tril_indices_from(A1, k=1)
+    A1_values = np.concatenate((A1[triu_indices], A1[tril_indices]))
+    A2_values = np.concatenate((A2[triu_indices], A2[tril_indices]))
+    m = len(A1_values)
     if method == "t":
         stat, pvalue = ttest_ind(
-            A1.ravel(), A2.ravel(), equal_var=False, alternative="two-sided"
+            A1_values, A2_values, equal_var=False, alternative="two-sided"
         )
-    elif method == "fisher":
-        # TODO should we replace with Fisher's exact test?
+    elif method == "chisq":
         raise NotImplementedError()
+    elif method == "fisher":
+        A1_n_edges = np.count_nonzero(A1_values)
+        A2_n_edges = np.count_nonzero(A2_values)
+        A1_n_nonedges = m - A1_n_edges
+        A2_n_nonedges = m - A2_n_edges
+
+        # row is graph
+        # cols are (no edge, edge)
+        cont_table = np.array(
+            [
+                [A1_n_nonedges, A1_n_edges],
+                [A2_n_nonedges, A2_n_edges],
+            ]
+        )
+        stat, pvalue = fisher_exact(cont_table, alternative="two-sided")
     return stat, pvalue, {}
--- scripts/test_perturbations_tests.py
+++ scripts/test_perturbations_tests.py
@@ -55,34 +55,68 @@ right_adj = binarize(right_adj)
 random_state = np.random.default_rng(8888)
 adj = right_adj
 n_sims = 2
-effect_sizes = [0, 50, 100, 150, 200]
+effect_sizes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 rows = []
 
 tests = {"ER": erdos_reyni_test}
+test_options = {"ER": [{"method": "t"}, {"method": "fisher"}]}
 perturbations = {"Remove edges": remove_edges}
 
 for perturbation_name, perturb in perturbations.items():
+
     for effect_size in effect_sizes:
         for sim in range(n_sims):
             seed = get_random_seed(random_state)
             perturb_adj = perturb(adj, effect_size=effect_size, random_state=seed)
             for test_name, test in tests.items():
-                stat, pvalue, other = test(adj, perturb_adj)
-                row = {
-                    "stat": stat,
-                    "pvalue": pvalue,
-                    "other": other,
-                    "test": test_name,
-                    "perturbation": perturbation_name,
-                    "effect_size": effect_size,
-                    "sim": sim,
-                }
-                rows.append(row)
+                option_sets = test_options[test_name]
+                for options in option_sets:
+                    stat, pvalue, other = test(adj, perturb_adj, **options)
+                    row = {
+                        "stat": stat,
+                        "pvalue": pvalue,
+                        "other": other,
+                        "test": test_name,
+                        "perturbation": perturbation_name,
+                        "effect_size": effect_size,
+                        "sim": sim,
+                        **options,
+                    }
+                    rows.append(row)
 
 results = pd.DataFrame(rows)
 #%%
 results
 
 #%%
-fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-sns.histplot(data=results, ax=ax, x="pvalue", hue="effect_size", binwidth=0.001)
+fig, axs = plt.subplots(2, 1, figsize=(8, 8), sharex=True)
+ax = axs[0]
+sns.histplot(
+    data=results[results["method"] == "t"],
+    ax=ax,
+    x="pvalue",
+    hue="effect_size",
+    binwidth=0.001,
+    legend=False,
+)
+ax.set_title("t-test")
+
+ax = axs[1]
+sns.histplot(
+    data=results[results["method"] == "fisher"],
+    ax=ax,
+    x="pvalue",
+    hue="effect_size",
+    binwidth=0.001,
+    legend=False,
+)
+ax.set_title("Fisher's exact")
+plt.tight_layout()
+
+#%%
+fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+sns.scatterplot(data=results, x="effect_size", y="pvalue", hue="method", ax=ax)
+sns.lineplot(
+    data=results, x="effect_size", y="pvalue", hue="method", ax=ax, legend=False
+)
+ax.set_xlabel("Number of edges removed")
