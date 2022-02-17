52c36bcbc07138eb2d71c927571eeecfc1b5256c
bdpedigo <benjamindpedigo@gmail.com>
2022-02-16 20:10:25 -0500

    rename script

--- shell/copy_results.sh
+++ shell/copy_results.sh
@@ -1,3 +1,4 @@
 (cd bilateral-connectome/overleaf && git pull)
 rsync -r --max-size=49m ./bilateral-connectome/results/figs ./bilateral-connectome/overleaf
+python ./bilateral-connectome/docs/unglue_variables.py
 (cd bilateral-connectome/overleaf && git add . && git commit -m 'update figures' && git push)
