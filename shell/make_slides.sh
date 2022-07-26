744c0d4a8d783154ac1092a7b6df65bc08792eb6
bdpedigo <benjamindpedigo@gmail.com>
2022-07-26 13:18:44 -0400

    cleanup

--- shell/make_slides.sh
+++ shell/make_slides.sh
@@ -5,11 +5,11 @@ SLIDE_DIR=$BASE_DIR/docs/slides/$1
 FILE=$SLIDE_DIR/$1.md
 
 if test -f "$FILE"; then
-    echo "$FILE exists."
+    echo "$FILE exists." 
+    marp --theme $SLIDE_DIR/../themes/slides.css --preview --html --allow-local-files $FILE
     # REF: https://github.com/orgs/marp-team/discussions/225
     # had timeout issues on some large presentations
-    PUPPETEER_TIMEOUT=60000 marp --pdf --allow-local-files $FILE 
-    marp --html --allow-local-files $FILE
+    PUPPETEER_TIMEOUT=45000 marp --theme $SLIDE_DIR/../themes/slides.css --pdf --allow-local-files $FILE
 else
     echo "$FILE does not exist."
 fi
