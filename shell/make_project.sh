501e3c250681aa4cf79390572883dd47154ca74d
bdpedigo <benjamindpedigo@gmail.com>
2022-09-16 14:06:04 -0700

    add opts to make project

--- shell/make_project.sh
+++ shell/make_project.sh
@@ -1,8 +1,20 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BASE_DIR=$SCRIPT_DIR/..
 
-export RESAVE_DATA=True
 
+while getopts 'r' opt; do
+    case $opt in 
+        r) 
+            export RERUN_SIMS=true
+            export RESAVE_DATA=true
+            echo "[make_project.sh] Set to rerun simulations and resaving data"
+        ;;
+    esac
+done
+
+shift $((OPTIND -1))
+
+echo "[make_project.sh] Running notebooks via make_notebook.sh"
 cat $BASE_DIR/scripts/manifest.txt | xargs -I % sh $SCRIPT_DIR/make_notebook.sh %
 
 # $SCRIPT_DIR/copy_results.sh
