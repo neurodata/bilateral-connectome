3a06f604e5b2d99eb5533fa2284d01220512dd15
bdpedigo <benjamindpedigo@gmail.com>
2022-09-16 14:06:44 -0700

    print out environment vars always

--- pkg/pkg/io/environ.py
+++ pkg/pkg/io/environ.py
@@ -13,4 +13,11 @@ def get_environment_variables():
     RESAVE_DATA = cast_to_bool(os.environ.get("RESAVE_DATA", False))
     RERUN_SIMS = cast_to_bool(os.environ.get("RERUN_SIMS", False))
     DISPLAY_FIGS = cast_to_bool(os.environ.get("DISPLAY_FIGS", True))
+
+    print("Environment variables:")
+    print("   RESAVE_DATA:", RESAVE_DATA)
+    print("   RERUN_SIMS:", RERUN_SIMS)
+    print("   DISPLAY_FIGS:", DISPLAY_FIGS)
+    print()
+
     return RESAVE_DATA, RERUN_SIMS, DISPLAY_FIGS
