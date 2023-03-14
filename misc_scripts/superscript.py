96b104e63018073c791469679c3634640fb54a89
Benjamin Pedigo <benjamindpedigo@gmail.com>
2023-03-14 09:27:41 -0400

    Delete misc_scripts directory

--- misc_scripts/superscript.py
+++
@@ -1,7 +0,0 @@
-#%%
-
-import matplotlib.pyplot as plt 
-
-fig, ax = plt.subplots(1,1, figsize=(10,10))
-
-ax.text(0.5, 0.5, r"DN$^{\mathrm{VNC}}$")
--- misc_scripts/test_environ.py
+++
@@ -1,9 +0,0 @@
-#%% [markdown]
-# # this is a test
-#%%
-from pkg.io import get_environment_variables
-
-_, RERUN_SIMS, DISPLAY_FIGS = get_environment_variables()
-
-print(RERUN_SIMS)
-print(DISPLAY_FIGS)
